e23626add747f19a960b6cf4ffd5d2b6ca82766d
Hou-Chen <houchen691@gmail.com>
2017-05-08 22:33:44 +0800

    2017/05/08

--- WatchWeb/WatchWeb/main/views.py
+++ WatchWeb/WatchWeb/main/views.py
@@ -13,16 +13,10 @@ def ARMANI(request):
     return render(request, 'main/ARMANI/ARMANI.html')
 def SEIKO(request):
     return render(request, 'main/SEIKO/SEIKO.html')
-def CHOPARD(request):
-    return render(request, 'main/CHOPARD/CHOPARD.html')
 def IWC(request):
     return render(request, 'main/IWC/IWC.html')
-def IWC_0321(request):
-    return render(request, 'main/IWC/IWC_0321.html')
 def CITIZEN(request):
     return render(request, 'main/CITIZEN/CITIZEN.html')
-def HAMILTON(request):
-    return render(request, 'main/HAMILTON/HAMILTON.html')
 def MIDO(request):
     return render(request, 'main/MIDO/MIDO.html')
 def TAGHeuer(request):
@@ -43,5 +37,9 @@ def PANERAI(request):
     return render(request, 'main/PANERAI/PANERAI.html')
 def TISSOT(request):
     return render(request, 'main/TISSOT/TISSOT.html')
+def OTHER(request):
+    return render(request, 'main/OTHER/OTHER.html')
+def OTHER_0321(request):
+    return render(request, 'main/OTHER/OTHER_0321.html')
 def email(request):    
     return render(request, 'main/email.html', send.send(request))
